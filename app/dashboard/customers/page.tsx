7bd466edc30486e747514ac4d0341420fe291a76
SamChukwuemeka <engrsamchukwuemeka@gmail.com>
2024-09-26 00:14:07 +0100

    final

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from 'next';
 
-const Page = () => {
-  return <p>Customers</p>;
+export const metadata: Metadata = {
+  title: 'Customers',
 };
 
-export default Page;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <main>
+      <CustomersTable customers={customers} />
+    </main>
+  );
+}
